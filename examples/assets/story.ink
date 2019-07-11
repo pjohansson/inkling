dfdee46d932ca427c7073ddcccf9dd16d5490cb1
Petter Johansson <pettjoha@kth.se>
2019-07-11 18:58:33 +0200

    Added minimum viable example of running a story

---
+++ examples/assets/story.ink
@@ -0,0 +1,33 @@
+// Story copied from Inkle's writing tutorial: 
+// https://www.inklestudios.com/ink/web-tutorial/
+
+LONDON, 1872
+Residence of Monsieur Phileas Fogg.
+-> london
+
+=== london ===
+Monsieur Phileas Fogg returned home early from the Reform Club, and in a new-fangled steam-carriage, besides!  
+"Passepartout," said he. "We are going around the world!"
+
++ "Around the world, Monsieur?"
+    I was utterly astonished. 
+    -> astonished
++ [Nod curtly.] -> nod
+
+
+=== astonished ===
+"You are in jest!" I told him in dignified affront. "You make mock of me, Monsieur."
+"I am quite serious."
+
++ "But of course"
+    -> ending
+
+
+=== nod ===
+I nodded curtly, not believing a word of it.
+-> ending
+
+
+=== ending
+"We shall circumnavigate the globe within eighty days." He was quite calm as he proposed this wild scheme. "We leave for Paris on the 8:25. In an hour."
+-> END
---
+++ examples/player.rs
@@ -0,0 +1,90 @@
+use std::{
+    env::current_dir,
+    fs::read_to_string,
+    io,
+    path::{Path, PathBuf},
+    process::exit,
+};
+
+use inkling::*;
+
+fn main() -> Result<(), io::Error> {
+    let base_dir = current_dir().unwrap();
+
+    let mut assets_dir = base_dir.clone();
+    assets_dir.push("examples");
+    assets_dir.push("assets");
+
+    let path: PathBuf = [assets_dir.as_path(), Path::new("story.ink")].iter().collect();
+    let story = read_story(&path)?;
+
+    match play_story(story) {
+        Ok(_) => println!("FIN\n"),
+        Err(err) => {
+            eprintln!("error: {}", err);
+            exit(1);
+        }
+    }
+
+    Ok(())
+}
+
+fn play_story(mut story: Story) -> Result<(), InklingError> {
+    let mut line_buffer = Vec::new();
+    let mut result = story.start(&mut line_buffer)?;
+    println!("{}", copy_lines_into_string(&line_buffer));
+
+    while let Prompt::Choice(choices) = result {
+        line_buffer.clear();
+
+        let choice = ask_user_for_choice(&choices).unwrap_or_else(|| {
+            println!("Exiting program.");
+            exit(0);
+        });
+
+        println!("");
+        result = story.resume_with_choice(choice, &mut line_buffer)?;
+        println!("{}", copy_lines_into_string(&line_buffer));
+    }
+
+    Ok(())
+}
+
+fn ask_user_for_choice(choices: &[Choice]) -> Option<&Choice> {
+    println!("Choose:");
+
+    for (i, choice) in choices.iter().enumerate() {
+        println!("  {}. {}", i + 1, choice.text);
+    }
+
+    println!("     ---");
+    println!("  0. Exit story");
+    println!("");
+
+    let index = get_choice(choices.len())?;
+    Some(&choices[index])
+}
+
+fn get_choice(num_choices: usize) -> Option<usize> {
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        match input.trim().parse::<usize>() {
+            Ok(0) => {
+                return None;
+            },
+            Ok(i) if i > 0 && i <= num_choices => {
+                return Some(i - 1);
+            },
+            _ => {
+                println!("Not a valid option, try again:");
+            }
+        }
+    }
+}
+
+fn read_story(path: &Path) -> Result<Story, io::Error> {
+    let contents = read_to_string(path)?;
+    Ok(read_story_from_string(&contents).unwrap())
+}
