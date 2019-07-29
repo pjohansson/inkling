0f0f7eef749af33e1bef52e27a28dc9500ecff66
Petter Johansson <pettjoha@kth.se>
2019-07-29 21:09:58 +0200

    Add integration tests for address validation

---
+++ tests/address_validation.rs
@@ -0,0 +1,132 @@
+use inkling::*;
+use inkling::error::ParseError;
+
+#[test]
+fn knot_names_in_diverts_are_validated() {
+    let content = "
+    
+== duckburg
+-> bin
+
+== money_bin
+-> END
+
+";
+
+    match read_story_from_string(content) {
+        Err(ParseError::InvalidAddress(..)) => (),
+        _ => panic!(),
+    }
+}
+
+#[test]
+fn stitch_names_in_diverts_are_validated() {
+    let content = "
+
+-> duckburg.bin
+    
+== duckburg
+= money_bin
+-> END
+
+";
+
+    match read_story_from_string(content) {
+        Err(ParseError::InvalidAddress(..)) => (),
+        _ => panic!(),
+    }
+}
+
+#[test]
+fn stitch_names_to_relative_addresses_are_validated() {
+    let content = "
+
+== duckburg
+Welcome to Duck Burg!
+-> bin
+
+= money_bin
+-> END
+
+";
+
+    match read_story_from_string(content) {
+        Err(ParseError::InvalidAddress(..)) => (),
+        _ => panic!(),
+    }
+}
+
+#[test]
+fn diverts_in_choice_text_are_validated() {
+    let content = "
+
+== duckburg
+Welcome to Duck Burg!
+*   [Money bin] -> bin
+
+= money_bin
+-> END
+
+";
+
+    match read_story_from_string(content) {
+        Err(ParseError::InvalidAddress(..)) => (),
+        _ => panic!(),
+    }
+}
+
+#[test]
+fn diverts_in_alternative_sequences_are_validated() {
+    let content = "
+
+== duckburg
+Welcome to Duck Burg! {We live here.|We headed to Uncle Scrooge's money bin. -> bin}
+
+== money_bin
+-> END
+
+";
+
+    match read_story_from_string(content) {
+        Err(ParseError::InvalidAddress(..)) => (),
+        _ => panic!(),
+    }
+}
+
+#[test]
+fn diverts_in_nested_branches_are_validated() {
+    let content = "
+
+== duckburg
+*   Money bin
+    We headed to the money bin. -> bin
+
+== money_bin
+-> END
+
+";
+
+    match read_story_from_string(content) {
+        Err(ParseError::InvalidAddress(..)) => (),
+        _ => panic!(),
+    }
+}
+
+#[test]
+fn condition_addresses_are_validated() {
+    let content = "
+
+== duckburg
+*   {bin} But we had already visited the money bin.
+*   -> END
+
+== money_bin
+-> END
+
+";
+
+    match read_story_from_string(content) {
+        Err(ParseError::InvalidAddress(..)) => (),
+        _ => panic!(),
+    }
+}
