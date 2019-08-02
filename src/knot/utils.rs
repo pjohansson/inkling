97e603996b327bee4fec88dee8ff6e2eb9aab9a1
Petter Johansson <pettjoha@kth.se>
2019-08-02 10:15:16 +0200

    Rename variable in `get_stitch`

--- src/knot/utils.rs
+++ src/knot/utils.rs
@@ -4,16 +4,16 @@ use crate::{
 };
 
 /// Return a reference to the `Stitch` at the target address.
-pub fn get_stitch<'a>(target: &Address, knots: &'a KnotSet) -> Result<&'a Stitch, InternalError> {
-    let knot_name = target.get_knot()?;
-    let stitch_name = target.get_stitch()?;
+pub fn get_stitch<'a>(address: &Address, knots: &'a KnotSet) -> Result<&'a Stitch, InternalError> {
+    let knot_name = address.get_knot()?;
+    let stitch_name = address.get_stitch()?;
 
     knots
         .get(knot_name)
         .and_then(|knot| knot.stitches.get(stitch_name))
         .ok_or(
             StackError::BadAddress {
-                address: target.clone(),
+                address: address.clone(),
             }
             .into(),
         )
@@ -21,18 +21,18 @@ pub fn get_stitch<'a>(target: &Address, knots: &'a KnotSet) -> Result<&'a Stitch
 
 /// Return a mutable reference to the `Stitch` at the target address.
 pub fn get_mut_stitch<'a>(
-    target: &Address,
+    address: &Address,
     knots: &'a mut KnotSet,
 ) -> Result<&'a mut Stitch, InternalError> {
-    let knot_name = target.get_knot()?;
-    let stitch_name = target.get_stitch()?;
+    let knot_name = address.get_knot()?;
+    let stitch_name = address.get_stitch()?;
 
     knots
         .get_mut(knot_name)
         .and_then(|knot| knot.stitches.get_mut(stitch_name))
         .ok_or(
             StackError::BadAddress {
-                address: target.clone(),
+                address: address.clone(),
             }
             .into(),
         )
