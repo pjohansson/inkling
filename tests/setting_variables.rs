ef8c7b1528f189727be22273b53fd1327de1887c
Petter Johansson <pettjoha@kth.se>
2019-08-03 22:14:42 +0200

    Add integration test for setting variables

--- tests/setting_variables.rs
+++ tests/setting_variables.rs
@@ -22,3 +22,28 @@ The latest measurement is {value} {unit}.
         "The latest measurement is 3.6 Röntgen.\n"
     );
 }
+
+#[test]
+fn global_variables_can_be_changed_from_the_valler() {
+    let content = "
+
+VAR value = 3.6
+VAR unit = \"Röntgen\"
+VAR is_hazardous = false
+
+The latest measurement is {value} {unit}. 
+
+";
+
+    let mut story = read_story_from_string(content).unwrap();
+    let mut line_buffer = Vec::new();
+
+    story.set_variable("value", 15000.0).unwrap();
+
+    story.start(&mut line_buffer).unwrap();
+
+    assert_eq!(
+        &line_buffer[0].text,
+        "The latest measurement is 15000 Röntgen.\n"
+    );
+}
