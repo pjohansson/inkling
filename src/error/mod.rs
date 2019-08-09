0c7f5bb2b31b3278f4c79ea1f1354293a857c8c5
Petter Johansson <pettjoha@kth.se>
2019-08-09 16:47:58 +0200

    Reexport `MetaData` in `error` module

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -3,11 +3,12 @@
 #[macro_use]
 mod error;
 pub mod parse;
-pub mod utils;
+pub(crate) mod utils;
 
 pub(crate) use error::IncorrectNodeStackError;
 pub use error::{InklingError, VariableError, VariableErrorKind};
 pub use parse::ReadError;
+pub use utils::MetaData;
 
 pub(crate) use error::{InternalError, ProcessError, ProcessErrorKind, StackError};
 pub(crate) use parse::{
--- src/error/utils.rs
+++ src/error/utils.rs
@@ -29,10 +29,13 @@ impl From<()> for MetaData {
     }
 }
 
-pub fn write_line_content<W: fmt::Write>(buffer: &mut W, line: &str) -> fmt::Result {
+pub(crate) fn write_line_content<W: fmt::Write>(buffer: &mut W, line: &str) -> fmt::Result {
     write!(buffer, " (line was: '{}'", line)
 }
 
-pub fn write_line_information<W: fmt::Write>(buffer: &mut W, meta_data: &MetaData) -> fmt::Result {
+pub(crate) fn write_line_information<W: fmt::Write>(
+    buffer: &mut W,
+    meta_data: &MetaData,
+) -> fmt::Result {
     write!(buffer, "(line {}) ", meta_data.line_index + 1)
 }
