2cbbdc2f1b9a3b84bcd7fbe2df26ef11f8957b71
Petter Johansson <pettjoha@kth.se>
2019-08-09 16:50:19 +0200

    Remove unused `write_line_content` function

--- src/error/utils.rs
+++ src/error/utils.rs
@@ -13,6 +13,14 @@ pub struct MetaData {
     pub line_index: u32,
 }
 
+/// Write meta data information for a line or piece of content in a story.
+pub(crate) fn write_line_information<W: fmt::Write>(
+    buffer: &mut W,
+    meta_data: &MetaData,
+) -> fmt::Result {
+    write!(buffer, "(line {}) ", meta_data.line_index + 1)
+}
+
 #[cfg(test)]
 impl From<usize> for MetaData {
     fn from(line_index: usize) -> Self {
@@ -27,15 +35,4 @@ impl From<()> for MetaData {
     fn from(_: ()) -> Self {
         MetaData { line_index: 0 }
     }
-}
-
-pub(crate) fn write_line_content<W: fmt::Write>(buffer: &mut W, line: &str) -> fmt::Result {
-    write!(buffer, " (line was: '{}'", line)
-}
-
-pub(crate) fn write_line_information<W: fmt::Write>(
-    buffer: &mut W,
-    meta_data: &MetaData,
-) -> fmt::Result {
-    write!(buffer, "(line {}) ", meta_data.line_index + 1)
 }
