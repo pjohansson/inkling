a9c6e044013280c008e840c2abaddd34db33ca03
Petter Johansson <pettjoha@kth.se>
2020-09-20 01:38:36 +0200

    Document more usage in `docs` module

--- src/docs/features.rs
+++ src/docs/features.rs
@@ -26,10 +26,22 @@
 //! 
 //! Text is separated into paragraphs by being on different lines.
 //! 
-//! ```plain
+//! ```
+//! # use inkling::{read_story_from_string, Prompt};
+//! # let content = r"
+//! #
 //! My hand moved towards the canvas.
 //! The cold draft made a shudder run through my body.
 //! A dark blot spread from where my pen was resting.
+//! #
+//! # ";
+//! # let mut story = read_story_from_string(content).unwrap();
+//! # let mut buffer = Vec::new();
+//! # story.start().unwrap();
+//! # story.resume(&mut buffer).unwrap();
+//! # assert_eq!(buffer[0].text, "My hand moved towards the canvas.\n");
+//! # assert_eq!(buffer[1].text, "The cold draft made a shudder run through my body.\n");
+//! # assert_eq!(buffer[2].text, "A dark blot spread from where my pen was resting.\n");
 //! ```
 //! 
 //! Those three lines will be returned from `inkling` as separate lines, each ending with
@@ -40,9 +52,21 @@
 //! The text file can contain comments, which will be ignored by `inkling` as it parses the story.
 //! To write a comment, preceed the line with `//`.
 //! 
-//! ```plain
+//! ```
+//! # use inkling::{read_story_from_string, Prompt};
+//! # let content = r"
+//! #
 //! The cold could not be ignored.
 //! // Unlike this line, which will be 
+//! As will the end of this. // removed comment at end of line
+//! #
+//! # ";
+//! # let mut story = read_story_from_string(content).unwrap();
+//! # let mut buffer = Vec::new();
+//! # story.start().unwrap();
+//! # story.resume(&mut buffer).unwrap();
+//! # assert_eq!(buffer[0].text, "The cold could not be ignored.\n");
+//! # assert_eq!(buffer[1].text, "As will the end of this.\n");
 //! ```
 //! 
 //! Note that multiline comments with `/*` and `*/` are ***not*** currently supported.
@@ -51,9 +75,24 @@
 //! 
 //! To mark a choice in a branching dialogue, use the `*` marker.
 //! 
-//! ```plain
+//! ```
+//! # use inkling::{read_story_from_string, Prompt};
+//! # let content = r"
+//! #
 //! *   Choice 1
 //! *   Choice 2 
+//! #
+//! # ";
+//! # let mut story = read_story_from_string(content).unwrap();
+//! # let mut buffer = Vec::new();
+//! # story.start().unwrap();
+//! # match story.resume(&mut buffer).unwrap() {
+//! #   Prompt::Choice(choices) => {
+//! #       assert_eq!(choices[0].text, "Choice 1");
+//! #       assert_eq!(choices[1].text, "Choice 2");
+//! #   } 
+//! #   _ => unreachable!()
+//! # }
 //! ```
 //! 
 //! (The `+` marker can also be used, although this results in a different behavior
@@ -64,11 +103,31 @@
 //! 
 //! After making a choice, the story proceeds from lines below the choice. So this story:
 //! 
-//! ```plain
+//! ```
+//! # use inkling::{read_story_from_string, Prompt};
+//! # let content = r#"
+//! #
 //! A noise rang from the door.
 //! *   "Hello?" I shouted.
 //!     "Who's there?"
 //! *   I rose from the desk and walked over.
+//! #
+//! # "#;
+//! # let mut story = read_story_from_string(content).unwrap();
+//! # let mut buffer = Vec::new();
+//! # story.start().unwrap();
+//! # match story.resume(&mut buffer).unwrap() {
+//! #   Prompt::Choice(choices) => {
+//! #       assert_eq!(choices[0].text, r#""Hello?" I shouted."#);
+//! #       assert_eq!(choices[1].text, r"I rose from the desk and walked over.");
+//! #   } 
+//! #   _ => unreachable!()
+//! # }
+//! # story.make_choice(0).unwrap();
+//! # story.resume(&mut buffer).unwrap();
+//! # assert!(buffer[0].text.starts_with(r#"A noise rang from the door."#));
+//! # assert!(buffer[1].text.starts_with(r#""Hello?" I shouted."#));
+//! # assert!(buffer[2].text.starts_with(r#""Who's there?""#));
 //! ```
 //! 
 //! results in this "game" for the user (in this case picking the first option):
@@ -89,9 +148,26 @@
 //! text presented to the user. Text encased in square brackets `[]` will, however, 
 //! be ignored. Building on the previous example: 
 //! 
-//! ```plain
+//! ```
+//! # use inkling::{read_story_from_string, Prompt};
+//! # let content = r#"
+//! #
 //! *   ["Hello?" I shouted.]
 //!     "Who's there?"
+//! #
+//! # "#;
+//! # let mut story = read_story_from_string(content).unwrap();
+//! # let mut buffer = Vec::new();
+//! # story.start().unwrap();
+//! # match story.resume(&mut buffer).unwrap() {
+//! #   Prompt::Choice(choices) => {
+//! #       assert_eq!(choices[0].text, r#""Hello?" I shouted."#);
+//! #   } 
+//! #   _ => unreachable!()
+//! # }
+//! # story.make_choice(0).unwrap();
+//! # story.resume(&mut buffer).unwrap();
+//! # assert!(buffer[0].text.starts_with(r#""Who's there?""#));
 //! ```
 //! 
 //! becomes
@@ -112,8 +188,25 @@
 //! the brackets will appear in both choice and output text. This makes it easy to 
 //! build a simple choice text into a more presentable sentence for the story:
 //! 
-//! ```plain
-//! *   "Hello[?"],", I shouted. "Who's there?"
+//! ```
+//! # use inkling::{read_story_from_string, Prompt};
+//! # let content = r#"
+//! #
+//! *   "Hello[?"]," I shouted. "Who's there?"
+//! #
+//! # "#;
+//! # let mut story = read_story_from_string(content).unwrap();
+//! # let mut buffer = Vec::new();
+//! # story.start().unwrap();
+//! # match story.resume(&mut buffer).unwrap() {
+//! #   Prompt::Choice(choices) => {
+//! #       assert_eq!(choices[0].text, r#""Hello?""#);
+//! #   } 
+//! #   _ => unreachable!()
+//! # }
+//! # story.make_choice(0).unwrap();
+//! # story.resume(&mut buffer).unwrap();
+//! # assert!(buffer[0].text.starts_with(r#""Hello," I shouted. "Who's there?""#));
 //! ```
 //! 
 //! gives
@@ -130,7 +223,10 @@
 //! Dialogue branches can be nested, more or less infinitely. Just add extra `*` markers
 //! to specify the depth.
 //! 
-//! ```plain
+//! ```
+//! # use inkling::{read_story_from_string, Prompt};
+//! # let content = r"
+//! #
 //! *   Choice 1
 //!     * *     Choice 1.1
 //!     * *     Choice 1.2
@@ -138,12 +234,36 @@
 //! *   Choice 2
 //!     * *     Choice 2.1
 //!     * *     Choice 2.2
+//! #
+//! # ";
+//! # let mut story = read_story_from_string(content).unwrap();
+//! # let mut buffer = Vec::new();
+//! # story.start().unwrap();
+//! # story.resume(&mut buffer).unwrap();
+//! # story.make_choice(0).unwrap();
+//! # match story.resume(&mut buffer).unwrap() {
+//! #   Prompt::Choice(choices) => {
+//! #       assert_eq!(&choices[0].text, "Choice 1.1");
+//! #       assert_eq!(&choices[1].text, "Choice 1.2");
+//! #       story.make_choice(1).unwrap();
+//! #       match story.resume(&mut buffer).unwrap() {
+//! #           Prompt::Choice(choices) => {
+//! #               assert_eq!(&choices[0].text, "Choice 1.2.1");
+//! #           }
+//! #           _ => unreachable!()
+//! #       }
+//! #   }
+//! #   _ => unreachable!()
+//! # }
 //! ```
 //! 
 //! Any extra whitespace is just for readability. The previous example produces the exact 
 //! same tree as this, much less readable, example:
 //! 
-//! ```plain
+//! ```
+//! # use inkling::read_story_from_string;
+//! # let content_nowhitespace = r"
+//! #
 //! *Choice 1
 //! **Choice 1.1
 //! **Choice 1.2
@@ -151,6 +271,23 @@
 //! *Choice 2
 //! **Choice 2.1
 //! **Choice 2.2
+//! #
+//! # ";
+//! # let content_whitespace = r"
+//! #
+//! # *   Choice 1
+//! #     * *     Choice 1.1
+//! #     * *     Choice 1.2
+//! #             * * *   Choice 1.2.1
+//! # *   Choice 2
+//! #     * *     Choice 2.1
+//! #     * *     Choice 2.2
+//! #
+//! # ";
+//! #
+//! # let story_nowhitespace = read_story_from_string(content_nowhitespace).unwrap();
+//! # let story_whitespace = read_story_from_string(content_whitespace).unwrap();
+//! # assert_eq!(format!("{:?}", story_nowhitespace), format!("{:?}", story_whitespace));
 //! ```
 //! 
 //! ## Glue
@@ -158,9 +295,19 @@
 //! If you want to remove the newline character from in between lines, you can use the `<>` 
 //! marker which signifies *glue*. This:
 //! 
-//! ```plain
+//! ```
+//! # use inkling::read_story_from_string;
+//! # let content = r"
+//! #
 //! This line will <>
 //! be glued to this, without creating a new paragraph.
+//! #
+//! # ";
+//! # let mut story = read_story_from_string(content).unwrap();
+//! # let mut buffer = Vec::new();
+//! # story.start().unwrap();
+//! # story.resume(&mut buffer).unwrap();
+//! # assert!(!buffer[0].text.ends_with("\n"));
 //! ```
 //! 
 //! Becomes:
@@ -171,9 +318,19 @@
 //! 
 //! as will this, since glue can be put at either end:
 //! 
-//! ```plain
+//! ```
+//! # use inkling::read_story_from_string;
+//! # let content = r"
+//! #
 //! This line will 
 //! <> be glued to this, without creating a new paragraph.
+//! #
+//! # ";
+//! # let mut story = read_story_from_string(content).unwrap();
+//! # let mut buffer = Vec::new();
+//! # story.start().unwrap();
+//! # story.resume(&mut buffer).unwrap();
+//! # assert!(!buffer[0].text.ends_with("\n"));
 //! ```
 //! 
 //! For these examples glue doesn't do much, but it will be more useful once we introduce 
@@ -191,9 +348,16 @@
 //! A knot is denoted by beginning the line with at least two (2) `=` signs followed by 
 //! a name for the knot. On the following lines, the story text can resume.
 //! 
-//! ```plain
+//! ```
+//! # use inkling::read_story_from_string;
+//! # let content = r"
+//! #
 //! == stairwell
 //! I made my way down the empty stairwell.
+//! #
+//! # ";
+//! # let mut story = read_story_from_string(content).unwrap();
+//! # assert!(story.move_to("stairwell", None).is_ok());
 //! ```
 //! 
 //! The name ('stairwell' in the previous example) cannot contain spaces or non-alphanumeric 
@@ -201,9 +365,16 @@
 //! make it easier to identify knots in the document. This is identical to the previous
 //! example:
 //! 
-//! ```plain
+//! ```
+//! # use inkling::read_story_from_string;
+//! # let content = r"
+//! #
 //! === stairwell ===
 //! I made my way down the empty stairwell.
+//! #
+//! # ";
+//! # let mut story = read_story_from_string(content).unwrap();
+//! # assert!(story.move_to("stairwell", None).is_ok());
 //! ```
 //! 
 //! ### Non-latin characters
@@ -214,18 +385,48 @@
 //! Chinese, Japanese, Arabic, Cyrillic and other characters. Do let us know if you 
 //! find any exceptions.
 //! 
-//! ```plain
+//! ```
+//! # use inkling::read_story_from_string;
+//! # let content = r"
+//! #
 //! === عقدة ===
 //! These
 //! 
 //! === 매듭 ===
 //! are
 //! 
-//! === முடிச்சு ===
+//! === गांठ ===
 //! all
 //! 
 //! === 結 ===
 //! allowed.
+//! #
+//! # ";
+//! # let mut story = read_story_from_string(content).unwrap();
+//! # assert!(story.move_to("عقدة", None).is_ok());
+//! # assert!(story.move_to("매듭", None).is_ok());
+//! # assert!(story.move_to("गांठ", None).is_ok());
+//! # assert!(story.move_to("結", None).is_ok());
+//! ```
+//! 
+//! ## Stitches
+//! Knots may be further subdivided into *stitches*. These are denoted by single `=` markers.
+//! 
+//! ```
+//! # use inkling::read_story_from_string;
+//! # let content = r"
+//! #
+//! === garden ===
+//! = entrance
+//! A pale moonlight illuminated the garden.
+//! 
+//! = well
+//! The well stank of stagnant water. Is that an eel I see at the bottom?
+//! #
+//! # ";
+//! # let mut story = read_story_from_string(content).unwrap();
+//! # assert!(story.move_to("garden", Some("entrance")).is_ok());
+//! # assert!(story.move_to("garden", Some("well")).is_ok());
 //! ```
 //! 
 //! ## Diverts
@@ -233,7 +434,11 @@
 //! the story to continue from there. They are designated with the `->` marker followed 
 //! by the destination. 
 //! 
-//! ```plain
+//! ```
+//! # use inkling::read_story_from_string;
+//! # let content = r"
+//! # -> stairwell
+//! #
 //! === stairwell ===
 //! The stairs creaked as I descended.
 //! -> lower_floor
@@ -245,9 +450,63 @@
 //! === lower_floor ===
 //! On the bottom I found an unlocked door. 
 //! -> garden
+//! #
+//! # ";
+//! # let mut story = read_story_from_string(content).unwrap();
+//! # let mut buffer = Vec::new();
+//! # story.start().unwrap();
+//! # story.resume(&mut buffer).unwrap();
+//! # assert_eq!(story.get_current_location().unwrap(), ("garden".to_string(), None));
 //! ```
 //! 
 //! Diverts are automatically followed as they are encountered.
+//! 
+//! ### Diverts to stitches
+//! 
+//! Stitches inside knots can be diverted to using `knot.stitch` as a destination:
+//! 
+//! ```
+//! # use inkling::read_story_from_string;
+//! # let content = r"
+//! #
+//! -> garden.entrance
+//! #
+//! # === garden ===
+//! # = well
+//! # Unreachable.
+//! # = entrance 
+//! # A pale moonlight illuminated the garden.
+//! # ";
+//! # let mut story = read_story_from_string(content).unwrap();
+//! # let mut buffer = Vec::new();
+//! # story.start().unwrap();
+//! # story.resume(&mut buffer).unwrap();
+//! # assert!(buffer[0].text.starts_with("A pale moonlight illuminated the garden."));
+//! ```
+//! 
+//! Stitches within the same knot can be diverted to with only the stitch name:
+//! 
+//! ```
+//! # use inkling::read_story_from_string;
+//! # let content = r"
+//! # -> garden
+//! #
+//! === garden ===
+//! -> well
+//! 
+//! = entrance
+//! A pale moonlight illuminated the garden.
+//! 
+//! = well
+//! The well stank of stagnant water. 
+//! #
+//! # ";
+//! # let mut story = read_story_from_string(content).unwrap();
+//! # let mut buffer = Vec::new();
+//! # story.start().unwrap();
+//! # story.resume(&mut buffer).unwrap();
+//! # assert!(buffer[0].text.starts_with("The well stank of stagnant water."));
+//! ```
 //! 
 //! ### Ending the story with `-> END`
 //! `END` is a destination that signifies that the story has come to, well, an end. Use 
@@ -259,7 +518,10 @@
 //! 
 //! A common use of branches is to divert to other knots. 
 //! 
-//! ```plain
+//! ```
+//! # use inkling::read_story_from_string;
+//! # let content = r"
+//! #
 //! *   [Descend stairs] -> lower_floor
 //! *   [Return to desk] 
 //!     I sighed, wearily, and returned to my room.
@@ -270,44 +532,153 @@
 //! 
 //! === lower_floor ===
 //! On the bottom I found an unlocked door. 
-//! ```
-//! 
-//! ## Stitches
-//! Knots may be further subdivided into *stitches*. These are denoted by single `=` markers.
-//! 
-//! ```plain
-//! === garden ===
-//! = entering
-//! A pale moonlight illuminated the garden as I entered it.
-//! 
-//! = well
-//! The well stank of stagnant water. Is that an eel I see at the bottom?
-//! ```
-//! 
-//! These can be diverted to using `knot.stitch` as a destination:
-//! 
-//! ```plain
-//! -> garden.entrance
-//! ```
-//! 
-//! Stitches within the same knot can be diverted to with only the stitch name:
-//! 
-//! ```plain
-//! === garden ===
-//! -> well
-//! 
-//! = entrance
-//! A pale moonlight illuminated the garden as I entered it.
-//! 
-//! = well
-//! The well stank of stagnant water. Is that an eel I see at the bottom?
+//! #
+//! # ";
+//! # let mut story = read_story_from_string(content).unwrap();
+//! # let mut buffer = Vec::new();
+//! # story.start().unwrap();
+//! # story.resume(&mut buffer).unwrap();
+//! # story.make_choice(1).unwrap();
+//! # story.resume(&mut buffer).unwrap();
+//! # assert_eq!(story.get_current_location().unwrap(), ("desk".to_string(), None));
 //! ```
 //! 
 //! # Variables
 //! 
+//! Global variables can be declared in the script using the `VAR` keyword. They 
+//! must be declared before the first knot.
+//! 
+//! ```
+//! # use inkling::{read_story_from_string, Variable};
+//! # let content = r#"
+//! #
+//! VAR a_float = 1.0
+//! VAR a_int = 2
+//! VAR a_bool = false
+//! VAR a_string = "A String"
+//! VAR a_destination = "-> stairwell"
+//! #
+//! # "#;
+//! # let story = read_story_from_string(content).unwrap();
+//! # assert_eq!(story.get_variable("a_float").unwrap(), Variable::Float(1.0));
+//! # assert_eq!(story.get_variable("a_int").unwrap(), Variable::Int(2));
+//! # assert_eq!(story.get_variable("a_bool").unwrap(), Variable::Bool(false));
+//! # assert_eq!(story.get_variable("a_string").unwrap(), Variable::String("A String".to_string()));
+//! # assert!(story.get_variable("a_destination").is_ok());
+//! ```
+//! 
+//! As shown in this example, the variable type is automatically assigned from 
+//! the given value. Once assigned, a variable's type cannot be changed.
+//! 
+//! ## Using variables in text
+//! 
+//! Variables can be inserted into text by enclosing them in curly braces.
+//! 
+//! ```
+//! # use inkling::{read_story_from_string, Variable};
+//! # let content = r#"
+//! #
+//! VAR time = 11
+//! VAR moon = "gloomy"
+//! 
+//! The time was {time}. A {moon} moon illuminated the room.
+//! #
+//! # "#;
+//! # let mut story = read_story_from_string(content).unwrap();
+//! # let mut buffer = Vec::new();
+//! # story.start().unwrap();
+//! # story.resume(&mut buffer).unwrap();
+//! # assert_eq!(buffer[0].text, "The time was 11. A gloomy moon illuminated the room.\n");
+//! ```
+//! 
+//! ## Variable assignment
+//! 
+//! **It is not currently possible to assign variables in the script. Use `Story::set_variable`.**
+//! 
+//! ## Constant variables
+//! 
+//! Constant variables, whose values cannot be changed, are declared using the `CONST` keyword.
+//! 
+//! ```
+//! # use inkling::{read_story_from_string, Variable};
+//! # let content = r#"
+//! #
+//! CONST name = "d'Artagnan" // Constant variable, cannot be modified
+//! VAR rank = "Capitaine"    // Non-constant variable, can be changed
+//! #
+//! # "#;
+//! # let mut story = read_story_from_string(content).unwrap();
+//! # assert_eq!(story.get_variable("name").unwrap(), Variable::from("d'Artagnan"));
+//! # assert!(story.set_variable("name", "Aramis").is_err());
+//! ```
+//! 
 //! # Metadata
 //! 
-//! ## Tags
+//! Information about the story, knots or even individual lines can be marked with *tags*. All tags
+//! begin with the `#` marker.
+//! 
+//! Tags are stored as pure strings and can thus be of any form you want. `inkling` assigns no 
+//! meaning to them on its own, it's for you as the user to decide how to treat them.
 //! 
 //! ## Global tags
+//! 
+//! Tags in the preamble are global story tags. Here you can typically mark up metadata for the script.
+//! 
+//! ```
+//! # use inkling::{read_story_from_string, Variable};
+//! # let content = r#"
+//! #
+//! ## title: Inkling
+//! ## author: Petter Johansson
+//! #
+//! # "#;
+//! # let story = read_story_from_string(content).unwrap();
+//! # let tags = story.get_story_tags();
+//! # assert_eq!(&tags[0], "title: Inkling");
+//! # assert_eq!(&tags[1], "author: Petter Johansson");
+//! ```
+//! 
+//! ## Knot tags
+//! 
+//! Tags encountered in a knot before any content is parsed as tags belonging to that knot.
+//! 
+//! ```
+//! # use inkling::read_story_from_string;
+//! # let content = r#"
+//! #
+//! === stairwell ===
+//! ## sound: blowing_wind.ogg
+//! ## dark, quiet, dangerous
+//! I made my way down the empty stairwell.
+//! #
+//! # "#;
+//! # let story = read_story_from_string(content).unwrap();
+//! # let tags = story.get_knot_tags("stairwell").unwrap();
+//! # assert_eq!(&tags[0], "sound: blowing_wind.ogg");
+//! # assert_eq!(&tags[1], "dark, quiet, dangerous");
+//! ```
+//! 
+//! ## Line tags
+//! 
+//! Lines can be tagged by adding the tag after the line content. Multiple tags can 
+//! be set, separated by additional '#' markers.
+//! 
+//! ```
+//! # use inkling::read_story_from_string;
+//! # let content = r#"
+//! #
+//! A pale moonlight illuminated the garden. # sound: crickets.ogg
+//! The well stank of stagnant water. # smell, fall # sound: water_drip.ogg
+//! #
+//! # "#;
+//! # let mut story = read_story_from_string(content).unwrap();
+//! # let mut buffer = Vec::new();
+//! # story.start().unwrap();
+//! # story.resume(&mut buffer).unwrap();
+//! # let tags1 = &buffer[0].tags;
+//! # let tags2 = &buffer[1].tags;
+//! # assert_eq!(&tags1[0], "sound: crickets.ogg");
+//! # assert_eq!(&tags2[0], "smell, fall");
+//! # assert_eq!(&tags2[1], "sound: water_drip.ogg");
+//! ```
 //! 
--- src/docs/mod.rs
+++ src/docs/mod.rs
@@ -1,3 +1,6 @@
 //! Documentation on how to use `inkling`.
+//! 
+//! Examples in this module should all be tested under the hood, and thus guaranteed 
+//! to work. If they do not, please open an issue on the project web page to let us know.
 
 pub mod features;
