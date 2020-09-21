bc6d8c677e39868c375a89e54e25bbb4f292c8d7
Petter Johansson <pettjoha@kth.se>
2020-09-21 02:58:40 +0200

    New attempt to publish book

--- src/error/runtime/mod.rs
+++
@@ -1,7 +0,0 @@
-#[macro_use]
-pub(crate) mod error;
-pub(crate) mod internal;
-pub mod variable;
-
-pub use error::InklingError;
-pub use internal::InternalError;
--- src/story/validate/mod.rs
+++
@@ -1,6 +0,0 @@
-//! Validate story and variable names, addresses, expressions, and conditions.
-
-pub(self) mod namespace;
-pub(self) mod validate;
-
-pub use validate::{validate_story_content, KnotValidationInfo, ValidateContent, ValidationData};
