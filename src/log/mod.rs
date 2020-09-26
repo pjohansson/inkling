9312b478bf1b7ed7c6ac8d24e533a181a97a9a62
Petter Johansson <pettjoha@kth.se>
2020-09-26 22:58:39 +0200

    Update example for `log` module

--- src/log/mod.rs
+++ src/log/mod.rs
@@ -5,6 +5,36 @@
 //! over and inspected, or printed to string buffers or files using regular formatting tools.
 //! It is recommended that you inspect this log when running your software, to ensure that any
 //! unexpected behavior in the script is understood.
+//!
+//! # Example
+//! ## Printing log to standard error
+//! ```
+//! # use inkling::read_story_from_string;
+//! # let content = "Empty story.";
+//! let story = read_story_from_string(content).unwrap();
+//!
+//! for message in story.log.iter() {
+//!     eprintln!("{}", message);
+//! }
+//! ```
+//!
+//! ## To-do comments
+//! ```
+//! # use inkling::{log::MessageKind, read_story_from_string};
+//! let content = "\
+//! === arrival_at_château ===
+//! TODO: Finish initial scene.
+//! By 11 PM I had arrived at the mansion.
+//! ";
+//!
+//! let story = read_story_from_string(content).unwrap();
+//! assert_eq!(story.log.todo_comments.len(), 1);
+//!
+//! for comment in story.log.todo_comments.iter() {
+//!     eprintln!("{}", comment);
+//! }
+//! ```
+//!
 
 mod logger;
 mod message;
